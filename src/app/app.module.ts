3eb8084e3c1f5a881b476d10c2992c34e1c4715d
aszechlicki <aszechlicki@users.noreply.github.com>
2019-06-03 10:50:10 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { JsComponent } from "../js/js.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    JsComponent,
+    RouterModule.forRoot([
+      {
+        path: "js",
+        component: JsComponent
+      }
+    ])
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
